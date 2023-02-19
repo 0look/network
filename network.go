10048caf7d2dd6882827d79498c87e1cade23b32
zqhhh <sadeye@qq.com>
2023-02-19 22:48:31 +0800

    chore:移除多余的代码

--- network.go
+++ network.go
@@ -1,7 +1,6 @@
 package network
 
 import (
-	"encoding/binary"
 	"errors"
 	"net"
 	"time"
@@ -33,45 +32,4 @@ type Session interface {
 
 type Server interface {
 	Start(addr string) error
-}
-
-type Frame struct {
-	Body   []byte
-	Extend uint64
-	Len    uint64
-}
-
-func (frame *Frame) UnMarshal(data []byte) error {
-	var fb = FrameByte(data)
-	var dataLen int = len(fb)
-	if dataLen <= 8+8 {
-		return errors.New("bad data")
-	}
-	frame.Body = make([]byte, 0)
-
-	frame.Len = fb.getFrameLenValue()
-	frame.Extend = binary.BigEndian.Uint64(fb[dataLen-8-8 : dataLen-8])
-	frame.Body = fb[:dataLen-8-2]
-	return nil
-}
-
-type FrameByte []byte
-
-func (fb FrameByte) getFrameLenValue() uint64 {
-	return binary.BigEndian.Uint64(fb[len(fb)-8:])
-}
-
-func ScanSplit(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	if atEOF && len(data) == 0 {
-		return 0, nil, nil
-	}
-	var fb = FrameByte(data)
-	frameLen := fb.getFrameLenValue()
-	if frameLen == 0 {
-		return 0, nil, ErrFrameFormat
-	}
-	if len(data) < int(frameLen) {
-		return 0, nil, nil
-	}
-	return int(frameLen), data[0:frameLen], nil
 }
